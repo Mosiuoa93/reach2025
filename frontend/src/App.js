437da8ab0f5aff80784a11536917f850222e3405
Mosiuoa93 <215052764+Mosiuoa93@users.noreply.github.com>
2025-11-07 15:27:53 +0200

    Ensure CoupleRegistration route is committed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage';
 import ChoicePage from './pages/ChoicePage';
 import IndividualRegistration from './pages/IndividualRegistration';
 import GroupRegistration from './pages/GroupRegistration';
+import CoupleRegistration from './pages/CoupleRegistration';
 import ConfirmationPage from './pages/ConfirmationPage';
 import AdminDashboard from './pages/AdminDashboard';
 import AdminLogin from './pages/AdminLogin';
@@ -26,6 +27,7 @@ function App() {
         <Route path="/register" element={<ChoicePage />} />
         <Route path="/register/individual" element={<IndividualRegistration />} />
         <Route path="/register/group" element={<GroupRegistration />} />
+        <Route path="/register/couple" element={<CoupleRegistration />} />
         <Route path="/register/confirmation" element={<ConfirmationPage />} />
       <Route path="/admin/login" element={<AdminLogin />} />
       <Route path="/admin" element={<ProtectedAdminRoute />} />
